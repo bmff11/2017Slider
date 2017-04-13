e00f260f07547d3f9bffdb770cc223a8d3375fb6
Unknown <bmff11@gmail.com>
2017-04-13 10:46:55 -0400

    Class 4/13

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        var player = coll.gameObject.GetComponent<Player>();
+        if (player !=null) {
+            gameObject.SetActive(false);
+            FindObjectOfType<GM>().SetPoints(FindObjectOfType<GM>().GetPoints() + 1);
+        }
+    }
+
+}
---
+++ Assets/Scripts/FlyingBooster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBooster : MonoBehaviour {
+
+    float timeStarted = 0;
+    Player player;
+
+    public float 
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        var player = coll.gameObject.GetComponent<Player>();
+        if (player != null) {
+            player.canFly = true;
+            gameObject.GetComponent<Collider2D>().enabled = false;
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+
+            timeStarted = Time.time;
+        }
+    }
+
+    private void Update() {
+        if(timeStarted != 0 && timeStarted + 10 < Time.time) {
+            timeStarted = 0;
+            player.canFly = false;
+        }
+    }
+}
